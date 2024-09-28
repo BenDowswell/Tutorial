78c1765aa27364637c0249883823239021fb37db
BenDowswell <ben.dowswell2011@gmail.com>
2024-09-28 19:12:53 +0100

    New beggining files

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hey")
+	// Strings
+	var nameOne string = "Ben"
+	var nameTwo string = "Kam"
+	badname := "yoshi"
+	fmt.Println(nameOne)
+
+	fmt.Println(nameOne, nameTwo)
+
+	fmt.Println(nameOne, nameTwo, badname)
+
+	// Ints
+	var ageOne int = 20
+	var ageTwo = 40
+
+	fmt.Println(ageOne, ageTwo)
+}
